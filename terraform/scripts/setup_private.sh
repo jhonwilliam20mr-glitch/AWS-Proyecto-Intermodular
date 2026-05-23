46581bf5ce176fd5e341ef841b6a81940c62b996
RobertMarton04 <151923121+RobertMarton04@users.noreply.github.com>
2026-05-23 08:02:44 +0200

    Update setup_private.sh
    Dejar solo la base de datos

--- terraform/scripts/setup_private.sh
+++ terraform/scripts/setup_private.sh
@@ -12,18 +12,5 @@ services:
       - POSTGRES_PASSWORD=nc_pass
       - POSTGRES_DB=nextcloud
       - POSTGRES_USER=nextcloud
-  app:
-    image: nextcloud:latest
-    ports: ["80:80"]
-    environment:
-      - POSTGRES_HOST=db
-      - POSTGRES_DB=nextcloud
-      - POSTGRES_USER=nextcloud
-      - POSTGRES_PASSWORD=nc_pass
-      - NEXTCLOUD_ADMIN_USER=admin
-      - NEXTCLOUD_ADMIN_PASSWORD=ProyectoNC123
-      - NEXTCLOUD_TRUSTED_DOMAINS=*
-      # --- CONTROL DE ACCESO ESTRICTO ---
-      - TRUSTED_PROXIES=10.0.1.0/24  # Solo acepta peticiones que vengan de la subred pública (Nginx)
 EOF
 docker compose up -d
