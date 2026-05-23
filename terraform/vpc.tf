cbb71f1d8dd4930f01503f2a6240d0f42af21920
Julian Felipe Eraso Adarme <erasojulian155@gmail.com>
2026-05-23 19:48:12 +0200

    Update vpc.tf
    Corrección de sintaxis ";" y respetar la linea donde empieza y termina "{" , "}"

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -25,7 +25,10 @@ resource "aws_nat_gateway" "nat" {
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-  route { cidr_block = "0.0.0.0/0"; gateway_id = aws_internet_gateway.igw.id }
+  route {       
+                cidr_block = "0.0.0.0/0"
+                gateway_id = aws_internet_gateway.igw.id
+ }
 }
 
 resource "aws_route_table_association" "pub" {
@@ -35,7 +38,10 @@ resource "aws_route_table_association" "pub" {
 
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
-  route { cidr_block = "0.0.0.0/0"; nat_gateway_id = aws_nat_gateway.nat.id }
+  route {       
+                cidr_block = "0.0.0.0/0"
+                nat_gateway_id = aws_nat_gateway.nat.id
+ }
 }
 
 resource "aws_route_table_association" "priv" {
