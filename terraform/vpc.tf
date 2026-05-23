9745b7663d0a081f2e51706ae46a26e6b883c9e0
Julian Felipe Eraso Adarme <erasojulian155@gmail.com>
2026-05-23 20:01:55 +0200

    Update vpc.tf
    sintaxis con terraform fmt

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,18 +1,18 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
 }
 
 resource "aws_internet_gateway" "igw" { vpc_id = aws_vpc.main.id }
 
 resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private" {
-  vpc_id = aws_vpc.main.id
+  vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
 }
 
@@ -25,26 +25,26 @@ resource "aws_nat_gateway" "nat" {
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-  route {       
-                cidr_block = "0.0.0.0/0"
-                gateway_id = aws_internet_gateway.igw.id
- }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 }
 
 resource "aws_route_table_association" "pub" {
-  subnet_id = aws_subnet.public.id
+  subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
-  route {       
-                cidr_block = "0.0.0.0/0"
-                nat_gateway_id = aws_nat_gateway.nat.id
- }
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
 }
 
 resource "aws_route_table_association" "priv" {
-  subnet_id = aws_subnet.private.id
+  subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.private.id
 }
