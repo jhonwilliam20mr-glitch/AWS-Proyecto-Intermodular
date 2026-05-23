93a5e4475899acb582f131ee513db3e992a19b7a
RobertMarton04 <151923121+RobertMarton04@users.noreply.github.com>
2026-05-23 08:04:53 +0200

    Update instances.tf
    Cambiar instancias por cambio de funcionamiento de la red privada

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "public_node" {
   key_name               = "vockey"
 }
 
-# Instancia Privada 1 (Nextcloud Nodo 1 + Base de Datos Postgres Principal)
+# Instancia Privada 1 (Base de Datos Postgres Principal)
 resource "aws_instance" "private_node_1" {
   ami                    = "ami-02dfbd4ff395f2a1b"
   instance_type          = "t3.small"
@@ -19,13 +19,23 @@ resource "aws_instance" "private_node_1" {
   key_name               = "vockey"
 }
 
-# Instancia Privada 2 (Nextcloud Nodo 2 - Se conecta a la BD de la Instancia 1)
+# Instancia Privada 2 (Nextcloud Nodo 1 - Se conecta a la BD de la Instancia 1)
 resource "aws_instance" "private_node_2" {
   ami                    = "ami-02dfbd4ff395f2a1b"
   instance_type          = "t3.small"
   subnet_id              = aws_subnet.private.id
-  private_ip             = "10.0.2.200"
+  private_ip             = "10.0.2.110"
   user_data              = file("scripts/setup_private_2.sh")
+  vpc_security_group_ids = [aws_security_group.allow_internal.id]
+  key_name               = "vockey"
+}
+# Instancia Privada 2 (Nextcloud Nodo 2 - Se conecta a la BD de la Instancia 1)
+resource "aws_instance" "private_node_3" {
+  ami                    = "ami-02dfbd4ff395f2a1b"
+  instance_type          = "t3.small"
+  subnet_id              = aws_subnet.private.id
+  private_ip             = "10.0.2.120"
+  user_data              = file("scripts/setup_private_3.sh")
   vpc_security_group_ids = [aws_security_group.allow_internal.id]
   key_name               = "vockey"
 }
